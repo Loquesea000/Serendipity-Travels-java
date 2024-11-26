691765903146e65ba7fe633f13e3c79bd543e289
PAG WEB <loquesea6991@gmail.com>
2024-11-25 23:32:38 -0500

    Delete src directory

--- src/main/java/com/SerendipityTravels/app/Repository/DestinosRepository.java
+++
@@ -1,9 +0,0 @@
-package com.SerendipityTravels.app.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.SerendipityTravels.app.Entity.Destinos;
-
-public interface DestinosRepository extends JpaRepository<Destinos, Integer> {
-
-}
--- src/main/java/com/SerendipityTravels/app/Repository/PaquetesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.SerendipityTravels.app.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.SerendipityTravels.app.Entity.PaquetesTuristicos;
-
-public interface PaquetesRepository extends JpaRepository<PaquetesTuristicos, Integer> {
-
-}
